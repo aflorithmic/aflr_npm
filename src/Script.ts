4f5ea962a3f204a6743b75b07df3cef80824ed98
zeritte <zeritte@gmail.com>
2021-02-18 06:29:39 +0300

    update script class

--- src/Script.ts
+++ src/Script.ts
@@ -1,23 +1,35 @@
+import { Aflr } from "./Aflr";
 import { API_BASE_URL } from "./constants";
 import { AxiosPromise, RequestBase } from "./RequestBase";
+import { IConfig } from "./types";
 
-export class Script extends RequestBase {
-  constructor(apiKey: string) {
-    const surl: string = `${API_BASE_URL}/script`;
-    const purl: string = `${API_BASE_URL}/scripts`;
-    super(apiKey, { singularUrl: surl, pluralUrl: purl });
+const url: string = `${API_BASE_URL}/script`;
+
+export class ScriptClass {
+  private config!: IConfig;
+  private RequestClass!: RequestBase;
+
+  public getModuleName() {
+    return "Script";
+  }
+
+  public configure(config: IConfig = this.config) {
+    this.RequestClass = new RequestBase(config.apiKey, url);
   }
 
   public list(): Promise<AxiosPromise> {
-    return this.getRequest(true);
+    return this.RequestClass.getRequest(true);
   }
 
   public retrieve(scriptId: string): Promise<AxiosPromise> {
-    return this.getRequest(false, scriptId);
+    return this.RequestClass.getRequest(false, scriptId);
   }
 
   public create(data: any): Promise<AxiosPromise> {
-    // TODO: implement script params
-    return this.postRequest(data);
+    // TODO: implement script param types
+    return this.RequestClass.postRequest(data);
   }
 }
+
+export const Script = new ScriptClass();
+Aflr.register(Script);
