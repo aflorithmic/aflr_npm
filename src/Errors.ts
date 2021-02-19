5d912225c7a73295d867b9ea5e8d8729004b5ecf
zeritte <zeritte@gmail.com>
2021-02-19 22:59:53 +0300

    new error returning

--- src/Errors.ts
+++ src/Errors.ts
@@ -1,17 +1,17 @@
 import { ErrorTypes } from "./types";
 
-export const isInitializedError = (): Promise<never> => {
-  return Promise.reject(ErrorTypes.notInitialized);
+export const isInitializedError = (): void => {
+  throw new Error(ErrorTypes.notInitialized);
 };
 
-export const isSubmoduleAlreadyInitializedError = (): Promise<never> => {
-  return Promise.reject(ErrorTypes.alreadyInitializedSubmodule);
+export const isSubmoduleAlreadyInitializedError = (): void => {
+  throw new Error(ErrorTypes.alreadyInitializedSubmodule);
 };
 
-export const isModuleAlreadyInitializedError = (): Promise<never> => {
-  return Promise.reject(ErrorTypes.alreadyInitializedModule);
+export const isModuleAlreadyInitializedError = (): void => {
+  throw new Error(ErrorTypes.alreadyInitializedModule);
 };
 
-export const isValidApiKeyError = (): Promise<never> => {
-  return Promise.reject(ErrorTypes.validApiKey);
+export const isValidApiKeyError = (): void => {
+  throw new Error(ErrorTypes.validApiKey);
 };
