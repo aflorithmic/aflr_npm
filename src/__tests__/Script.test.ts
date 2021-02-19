5a2dacd7ae667357f9ed59bc70fe5226c4902186
zeritte <zeritte@gmail.com>
2021-02-19 15:30:11 +0300

    finish script tests

--- src/__tests__/Script.test.ts
+++ src/__tests__/Script.test.ts
@@ -1,8 +1,90 @@
+import { exception } from "console";
 import Aflr, { Script } from "../index";
+require("dotenv").config();
 
-test("It should have some properties", () => {
-  Aflr.configure({ apiKey: "some-api-key" });
-  expect(Script).toHaveProperty("list");
-  expect(Script).toHaveProperty("create");
-  expect(Script).toHaveProperty("retrieve");
+describe("Script operations", () => {
+  Aflr.configure({ apiKey: process.env.API_KEY, debug: true });
+  const testScriptText = "Hey testing!";
+  const testValues = "test";
+  let createdScriptId: string;
+
+  test("It should create a new script", async () => {
+    try {
+      // @ts-ignore
+      const { scriptId, scriptText, scriptName, moduleName, projectName } = await Script.create({
+        scriptText: testScriptText,
+        scriptName: testValues,
+        moduleName: testValues,
+        projectName: testValues
+      });
+      createdScriptId = scriptId;
+      expect(scriptText).toMatch(testScriptText); // since section names are added automatically, we dont do exact check
+      expect(scriptName).toBe(testValues);
+      expect(moduleName).toBe(testValues);
+      expect(projectName).toBe(testValues);
+    } catch (e) {
+      throw new Error("test failed");
+    }
+  });
+
+  test("It should retrieve the created script", async () => {
+    try {
+      // @ts-ignore
+      const { scriptText, scriptName, moduleName, projectName } = await Script.retrieve(
+        createdScriptId
+      );
+      expect(scriptText).toMatch(testScriptText); // since section names are added automatically, we dont do exact check
+      expect(scriptName).toBe(testValues);
+      expect(moduleName).toBe(testValues);
+      expect(projectName).toBe(testValues);
+    } catch (e) {
+      throw new Error("test failed");
+    }
+  });
+
+  test("It should list all of the scripts and find the created one", async () => {
+    try {
+      // @ts-ignore
+      const scripts = await Script.list();
+      expect(Array.isArray(scripts)).toBe(true);
+      expect(scripts).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            scriptText: expect.stringMatching(testScriptText),
+            scriptName: testValues,
+            moduleName: testValues,
+            projectName: testValues
+          })
+        ])
+      );
+    } catch (e) {
+      throw new Error("test failed");
+    }
+  });
+});
+
+describe("Script module initialization", () => {
+  test("It should return an error if not configured", async () => {
+    try {
+      await Script.list();
+    } catch (e) {
+      expect(e).toMatch(/configure the package before using it/);
+    }
+  });
+
+  test("It should not allow submodule configuration", async () => {
+    try {
+      Aflr.configure({ apiKey: "some-api-key" });
+      await Script.configure({ apiKey: "1", baseUrl: "1" });
+    } catch (e) {
+      expect(e).toMatch(/has already been initialized/);
+    }
+  });
+
+  test("It should have some properties", () => {
+    Aflr.configure({ apiKey: "some-api-key" });
+    expect(Script).toHaveProperty("list");
+    expect(Script).toHaveProperty("create");
+    expect(Script).toHaveProperty("retrieve");
+  });
 });
