4d6ea5a64ffb41067295aa1d0bc7a0add165a76f
zeritte <zeritte@gmail.com>
2021-02-19 17:00:59 +0300

    add voice tests

---
+++ src/__tests__/Voice.test.ts
@@ -0,0 +1,46 @@
+import Aflr, { Voice } from "../index";
+require("dotenv").config();
+
+describe("Voice module initialization", () => {
+  beforeEach(() => {
+    Aflr.reset();
+  });
+
+  test("It should return an error if not configured", async () => {
+    try {
+      await Voice.list();
+    } catch (e) {
+      expect(e).toMatch(/configure the package before using it/);
+    }
+  });
+
+  test("It should not allow submodule configuration", async () => {
+    try {
+      Aflr.configure({ apiKey: "some-api-key" });
+      await Voice.configure({ apiKey: "1", baseUrl: "1" });
+    } catch (e) {
+      expect(e).toMatch(/has already been initialized/);
+    }
+  });
+
+  test("It should have some properties", () => {
+    Aflr.configure({ apiKey: "some-api-key" });
+    expect(Voice).toHaveProperty("list");
+  });
+});
+
+describe("Speech operations", () => {
+  beforeEach(() => {
+    Aflr.reset();
+    Aflr.configure({ apiKey: process.env.API_KEY, debug: true });
+  });
+
+  test("It should list all of the scripts and find the created one", async () => {
+    try {
+      const voices = await Voice.list();
+      expect(Array.isArray(voices["voices"])).toBe(true);
+    } catch (e) {
+      throw new Error("test failed");
+    }
+  });
+});
