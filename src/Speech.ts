ee8f641ec2b044f0cb3f4674282c0b6a063cfae6
zeritte <zeritte@gmail.com>
2021-02-20 14:12:59 +0300

    add timeout config to speech retrieval

--- src/Speech.ts
+++ src/Speech.ts
@@ -24,7 +24,7 @@ export class SpeechClass {
     if (!this.#initialized) {
       isInitializedError();
     }
-    return this.#RequestClass.getRequest(scriptId);
+    return this.#RequestClass.getRequest(scriptId, { timeout: 30000 });
   }
 
   /**
