c1ef771c895692ff5573547ac5ecf9f3611c14ae
zeritte <zeritte@gmail.com>
2021-03-17 16:04:49 +0300

    export it

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { Aflr } from "./Aflr";
 import { Script } from "./Script";
 import { Speech } from "./Speech";
+import { SyncTTS } from "./SyncTTS";
 import { Voice } from "./Voice";
 
 Aflr.Script = Script;
 Aflr.Speech = Speech;
 Aflr.Voice = Voice;
+Aflr.SyncTTS = SyncTTS;
 
 export default Aflr;
-export { Script, Speech, Voice };
+export { Script, Speech, Voice, SyncTTS };
