f2078e8002e7746be8a6dc9580851f6e55db85e4
zeritte <zeritte@gmail.com>
2021-06-23 11:58:29 +0300

    better jsdocs

--- src/types.ts
+++ src/types.ts
@@ -93,9 +93,9 @@ export interface IVoiceFilteringBody {
   accent?: string;
   /** Try with one of: adult, child, senior */
   ageBracket?: string;
-  /** Try with one or more (separated by commas) of: steady, confident, balanced, informative, serious, instructional, slow, storytelling, calm, clear, deep, formal, sad, thin, fast, upbeat, fun, energetic, tense, very fast, flat, low pitched, high pitched, low-pitched, sing-y, cooperative, kind, stable, monotonous, neutral, responsible, business man, straight to the point, knowledgeable, focused, newscastery, newsreader, interviewer, reliable, friendly, welcoming, good for handing out information, slightly friendly */
+  /** Try with one or more (separated by commas) of: steady, confident, balanced, informative, serious, storytelling, calm, deep, formal, sad, thin, energetic, upbeat, fun, tense, flat, low pitched, high pitched, fast, slow */
   tags?: string;
-  /** Try with one or more (separated by commas) of: fitness, business, commercial, fashion, travel, audiobook, real estate, faith, health industry, comercial, realestate, kids entertainment, games, customer service, education, storytelling, entertainment, kids, education audiobook */
+  /** Try with one or more (separated by commas) of: fitness, business, commercial, fashion, travel, audiobook, real estate, faith, health industry, kids entertainment, games, customer service, education, storytelling, entertainment, kids */
   industryExamples?: string;
   [key: string]: any;
 }
